ba0271ce6af88ed0837cb80aa15615faff2ecea2
PL-coding <113569279+PL-coding@users.noreply.github.com>
2023-02-13 13:09:56 +1100

    Added form and searching functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-        <button className="btn btn-primary">Hello</button>
-      </header>
+              <h1>dictionary</h1>
+              </header>
+      <main className="container">
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by Louise Gray and open-sourced on <a href='https://github.com/PL-coding/dictionary-project' target="_blank" rel="noreferrer">GitHub</a></footer>
     </div>
   );
 }
