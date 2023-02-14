d85781e04976c61c9eb5196dcda5329026fc20a2
PL-coding <113569279+PL-coding@users.noreply.github.com>
2023-02-14 13:27:00 +1100

    Added dictionary API and API documentation link

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,9 +4,16 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+
+    function handleResponse(response) {
+        console.log(response);
+    }
+
+    // documentation https://www.shecodes.io/learn/workshops/1096/apis/dictionary
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=0deecee21740t22ba3fb3658doabb96f`
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event) {
